abbe78c2aa6891627080f910e5ad2fb9dda9f3b3
Miyorhokpor Kingdom <kabutey.kingdom@yahoo.com>
2023-02-22 15:02:15 -0800

    0-putchar

--- 0x02-funtions_nested_loops/0-putchar.c
+++ 0x02-funtions_nested_loops/0-putchar.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * main - Entry point
- * Return: Always 0
+ * Description: 'prints _putchar to stdout'
+ * Return: always 0
  */
-
 int main(void)
-
 {
 	_putchar('_');
 	_putchar('p');
@@ -17,6 +15,4 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
-
 }
