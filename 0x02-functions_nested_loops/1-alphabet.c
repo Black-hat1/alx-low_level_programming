4315b80cc92b5c3b76bb7f91195e621bd55fdb05
Miyorhokpor Kingdom <kabutey.kingdom@yahoo.com>
2023-02-22 18:18:35 -0800

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * print_alphabet - Prints alpha in lower case
+ * print_alphabet - Prints the alphabet in lowercase.
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
-	char c = 'a';
+	int i;
 
-	while (c <= 'z');
+	for (i = 97; i <= 122; i++)
 	{
-		_putchar(c);
-		c++;
+		_putchar(i);
 	}
 	_putchar('\n');
 }
