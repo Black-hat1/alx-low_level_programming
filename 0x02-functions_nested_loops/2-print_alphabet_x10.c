963f437df3fc39a5ec0e3dde9578c6e6bc8a8dc2
Miyorhokpor Kingdom <kabutey.kingdom@yahoo.com>
2023-02-22 18:56:16 -0800

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,23 +2,19 @@
 /**
  * print_alphabet_x10 - Prints the alphabet 10 times.
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
-	char ch;
 	int i;
+	int j;
 
-	i = 0;
-
-	while (i < 10)
+	for (i = 1; i <= 10; i++)
 	{
-		ch = 'a';
-		while (ch <= 'z')
+		for (j = 97; j <= 122; j++)
 		{
-			_putchar(ch);
-			ch++;
+			_putchar(j);
 		}
 		_putchar('\n');
-		i++;
+	}
 }
