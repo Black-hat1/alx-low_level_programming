e531c321ce2d6143ddf7f63814e2acdb478a2699
Miyorhokpor Kingdom <kabutey.kingdom@yahoo.com>
2023-02-27 08:32:32 -0800

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,12 +6,13 @@
  *
  * Return: The length of @str.
  */
-size_t _strlen(const char *str)
+int _strlen(char *s)
 {
-	size_t length = 0;
+	int count = 0;
 
-	while (*str++)
-		length++;
-
-	return (length);
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+	return (count);
 }
