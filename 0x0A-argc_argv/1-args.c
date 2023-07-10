eb81abea6ad25d48fe6697b25ceac7fa2f7abaab
Black-hat1 <kabutey.kingdom@yahoo.com>
2023-07-09 17:25:11 -0700

    Task 2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the arguments to be printed
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc
+	printf("%i\n", argc - 1);
+	return (0);
+}
