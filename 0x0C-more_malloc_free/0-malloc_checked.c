98619e94ce13e132f37858cce21cf552db4130fa
Black-hat1 <kabutey.kingdom@yahoo.com>
2023-07-13 08:02:54 -0700

    first commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory with malloc
+ * @a: size of memory to be allocated.
+ *
+ * Return: pointer to allocated memory
+ */
+void *malloc_checked(unsigned int a)
+{
+	void *p = malloc(a);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
