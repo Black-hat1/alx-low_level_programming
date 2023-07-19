e95cf2da62106bad5ace8d1527d02ca63195ac31
Black-hat1 <kabutey.kingdom@yahoo.com>
2023-07-19 05:24:16 -0700

    Bonus

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,10 +2,24 @@
 #include <stdio.h>
 
 /**
- * print_opcodes - print the opcodes of this program
- * @a: address of the main function
- * @n: number of bytes to print
+ * main - print the opcodes of this program
+ * @argc: the number of agruments
+ * @argv: the array of pointers
  *
  * Return: void
  */
+int main(int argc, char *argv[])
+{
+	int index;
 
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]) != 0)
+			return (index);
+	}
+
+	return (-1);
+}
