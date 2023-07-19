71267c686ee9559406200ca99d08185f129c5018
Black-hat1 <kabutey.kingdom@yahoo.com>
2023-07-19 05:03:18 -0700

    Task 1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,18 +9,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
-}
-
-void print_with_hello(char *name)
-{
-    printf("Hello, %s!\n", name);
-}
-
-int main()
-{
-    char name[] = "John";
-    print_name(name, print_with_hello);
-
-    return 0;
+	if (name != NULL && f != NULL)
+	f(name);
 }
