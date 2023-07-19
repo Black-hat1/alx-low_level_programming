711e5a048fb16123f752527776e9372c0cff462d
Black-hat1 <kabutey.kingdom@yahoo.com>
2023-07-19 04:42:24 -0700

    New

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name
+ * @name: name to print
+ * @f: funtion
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *));
+}
+
+void print_with_hello(char *name) {
+    printf("Hello, %s!\n", name);
+}
+
+int main() {
+    char name[] = "John";
+    print_name(name, print_with_hello);
+
+    return 0;
+}
