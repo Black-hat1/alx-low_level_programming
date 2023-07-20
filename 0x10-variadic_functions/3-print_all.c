5b965eeac4d2068773108d126f259486ca4fa788
Black-hat1 <kabutey.kingdom@yahoo.com>
2023-07-20 07:09:50 -0700

    Task print_all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,43 +3,49 @@
 #include <stdio.h>
 
 /**
- * print_all - prints anything
- * @format: list of argument types passed to the function
- *
- *
- * Return: void
+ * print_all - Variadic function that can print any argument passed in
+ * as long as it is a character, integer, float, or string
+ * @format: The data type being passed into the function
+ * Return: Nothing
  */
 void print_all(const char * const format, ...)
 {
-	va_list kingdom;
+	int i = 0;
+	va_list argument_input;
+	char *str = "(nil)";
+	char *sarray = NULL;
+	char *separator = ", ";
 
-	va_start(kingdom, format);
+	va_start(argument_input, format);
 
-	while (*format)
+	while (format[i] != '\0' && format != NULL)
 	{
-		switch (*format)
+		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(kingdom, int));
+				printf("%c", va_arg(argument_input, int));
 				break;
 			case 'i':
-				printf("%d",  va_arg(kingdom, int));
+				printf("%d", va_arg(argument_input, int));
 				break;
 			case 'f':
-				printf("%f", va_arg(kingdom, double));
+				printf("%f", va_arg(argument_input, double));
 				break;
 			case 's':
-				char *str_arg = va_arg(kingdom, char *);
-				if (str_arg != NULL)
-					printf("%s", str_arg);
-				else
-					printf("(nil)");
+				sarray = va_arg(argument_input, char *);
+				if (sarray == NULL)
+					sarray = str;
+				printf("%s", sarray);
 				break;
+			default:
+				i++;
+				continue;
 		}
-		format++;
+		if ((format[i + 1] != '\0') && (format[i] == 'c' || format[i] == 'i' ||
+					format[i] == 'f' || format[i] == 's'))
+			printf("%s", separator);
+		i++;
 	}
-
-	va_end(kingdom);
-
 	printf("\n");
+	va_end(argument_input);
 }
