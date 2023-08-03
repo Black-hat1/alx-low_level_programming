e90abee4cfaa3719101467371503a7dc41137586
Black-hat1 <kabutey.kingdom@yahoo.com>
2023-08-03 13:28:29 -0700

    Correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,23 +9,21 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
-
-	if (b == '\0')
+	if (b == NULL)
 		return (0);
 
-	for (len = 0; b[len];)
-		len++;
+	int len = strlen(b);
+	unsigned int result = 0;
 
-	for (len -= 1; len >= 0; len--)
+	for (int i = len - 1; i >= 0; i--)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[i] == '0')
+			result <<= 1;
+		else if (b[i] == '1')
+			result = (result << 1) | 1;
+		else
 			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
 	}
 
-	return (num);
+	return (result);
 }
